f2ddb83fc7a0af135b5a9a9dae687040e5321ff2
bernard.carvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-13 17:29:46 -0300

    commit incial do projeto feito com initializer

---
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.carvalho.bernard.mallify.mallify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.carvalho.bernard.mallify.mallify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
