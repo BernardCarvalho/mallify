f71235400c0190679bac53af17b93618fe7d042f
bernard.carvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-13 19:19:32 -0300

    adicionado codigo de teste na main

--- src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplication.java
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplication.java
@@ -3,11 +3,19 @@ package br.edu.ifto.carvalho.bernard.mallify.mallify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.edu.ifto.carvalho.bernard.mallify.mallify.Entity.Produto;
+
 @SpringBootApplication
 public class MallifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MallifyApplication.class, args);
+		Produto produto = new Produto();
+		produto.isValid();
+		System.out.println(produto.getErros());
+		
+		
+
 	}
 
 }
