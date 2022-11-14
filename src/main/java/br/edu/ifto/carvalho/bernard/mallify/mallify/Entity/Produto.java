5d50f84562fec8572213a6f5ca1dbeec67627648
bernard.carvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-14 12:36:43 -0300

    remoção de dependencia desnecessaria

--- src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/Entity/Produto.java
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/Entity/Produto.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import javax.validation.Validation;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Entity
