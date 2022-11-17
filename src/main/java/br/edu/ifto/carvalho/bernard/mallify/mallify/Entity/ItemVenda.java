7e1d6a79d4550e8e98f2fcfdad319b93f5438936
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-17 13:54:07 -0300

    adicionada interface Validable

--- src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/Entity/ItemVenda.java
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/Entity/ItemVenda.java
@@ -4,14 +4,18 @@ import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import br.edu.ifto.carvalho.bernard.mallify.mallify.Classes.EntityValidatorHelper;
+import br.edu.ifto.carvalho.bernard.mallify.mallify.Interfaces.Validable;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "tbl_itemvenda")
-public class ItemVenda implements Serializable {
+public class ItemVenda implements Serializable , Validable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
@@ -25,4 +29,17 @@ public class ItemVenda implements Serializable {
     @Min(1)
     private Double quantidade;
     
+    @Override
+    public Boolean isValid() {
+        EntityValidatorHelper<ItemVenda> entityValidatorHelper = new EntityValidatorHelper<>(this);
+        return entityValidatorHelper.isValid();
+    }
+
+
+    @Override
+    public Map<String, List<String>> getErros() {
+        EntityValidatorHelper<ItemVenda> entityValidatorHelper = new EntityValidatorHelper<>(this);
+        return entityValidatorHelper.getErros();        
+    }
+
 }
