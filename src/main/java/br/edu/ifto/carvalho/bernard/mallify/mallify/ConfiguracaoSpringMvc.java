642ba6d64cf1d8b20d635916d42020d50a4dc5b5
bernard <bernard.carvalho@ifto.edu.br>
2022-11-17 10:35:31 -0300

    alterações que permitem a homepage funcionar

---
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/ConfiguracaoSpringMvc.java
@@ -0,0 +1,21 @@
+package br.edu.ifto.carvalho.bernard.mallify.mallify;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Configuration para indicar ao Spring que essa é uma classe de configuração.
+ * Em seguida, é preciso implementar a interface WebMvcConfigurer.
+ * @author fagno
+ */
+
+@Configuration
+public class ConfiguracaoSpringMvc implements WebMvcConfigurer{
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/home");
+    }
+}
--- src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplication.java
+++ src/main/java/br/edu/ifto/carvalho/bernard/mallify/mallify/MallifyApplication.java
@@ -3,8 +3,8 @@ package br.edu.ifto.carvalho.bernard.mallify.mallify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.edu.ifto.carvalho.bernard.mallify.mallify.Entity.Produto;
-import br.edu.ifto.carvalho.bernard.mallify.mallify.Entity.Venda;
+//import br.edu.ifto.carvalho.bernard.mallify.mallify.Entity.Produto;
+//import br.edu.ifto.carvalho.bernard.mallify.mallify.Entity.Venda;
 
 @SpringBootApplication
 public class MallifyApplication {
@@ -12,15 +12,15 @@ public class MallifyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MallifyApplication.class, args);
 		
-		Produto produto = new Produto();
-		
-		produto.isValid();
-		System.out.println("Produto é valido: "+produto.isValid());
-		System.out.println(produto.getErros());
+		// Produto produto = new Produto();
+		// produto.isValid();
+		// System.out.println("Produto é valido: "+produto.isValid());
+		// System.out.println(produto.getErros());
 
-		Venda venda = new Venda();
-		System.out.println("Venda é valida: "+venda.isValid());
-		System.out.println(venda.getErros());
+		// Venda venda = new Venda();
+		// produto.isValid();
+		// System.out.println("Venda é valida: "+venda.isValid());
+		// System.out.println(venda.getErros());
 		
 		
 
